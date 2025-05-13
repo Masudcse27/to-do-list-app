5376cf8e22ae993d845a6f5f59a6002f52ae2e08
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-05-13 19:42:18 +0600

    Task create complete

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,11 @@
 <?php
 
-$host = 'Localhost';
+$host = 'localhost';
 $user_name = 'root';
 $password = '';
-$db_name = 'to_do_list';
+$db_name = 'to_do_list_app';
 
-$conn = new mysqli($host, $user_name, $password, $db_name);
+$conn = new mysqli($host,$user_name,$password,$db_name);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
+session_start();
 
+unset($_SESSION['errors']);
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +10,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <div class="container">
+        <form action="create_task.php" method="post">
+            <h1>Add a New Task</h1>
+            <?php if(isset($_SESSION['error'])): ?>
+                <p class="alert"><?php echo $_SESSION['error']; unset($_SESSION['error']) ?></p>
+            <?php endif; ?>
+            <div>
+                <label for="">Task Name</label><br>
+                <input class="input_round" type="text" name="task_name">
+            </div>
+            <div>
+                <label for="">Task Time</label><br>
+                <span><input class="input_round" type="number" name="task_time"> Hours</span>
+            </div>
+            <div >
+                <button class="btn" type="submit">Add Task</button>
+            </div>
+        </form>
+        <table>
+            <tr>
+                <th>Task</th>
+                <th>Task Time</th>
+                <th>Action</th>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
